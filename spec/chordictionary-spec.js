4367968aac6afad7875f5d34f981157fe190668b
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-11-15 20:06:11 -0300

    Add jasmine-defs.js for spec linting

--- spec/chordictionary-spec.js
+++ spec/chordictionary-spec.js
@@ -1,3 +1,5 @@
+import { _describe, _it } from "./support/jasmine-defs.js";
+
 import { chords } from "./chords.js";
 import {
     Instrument,
@@ -13,17 +15,17 @@ import {
 import { assert } from "chai";
 import { WORDING } from "../src/js/wordings.js";
 
-
 var guitar = new Instrument(tuning.guitar.standard.join(""), 24, 5, 4);
 
-describe("FOO BAR", () => {
-    it("chordictionary.isValidTab()", () => {
+
+_describe("FOO BAR", () => {
+    _it("chordictionary.isValidTab()", () => {
         assert.ok(isValidTab("x32010") === true, "x32010 is a valid tab.");
         assert.ok(isValidTab("911111099") === true, "911111099 is a valid tab (['9','11','11','10','9','9']).");
         assert.ok(isValidTab("x3201A") === false, "x3201A is not a valid tab.");
     });
 
-    it("chordictionary.isValidTuning()", () => {
+    _it("chordictionary.isValidTuning()", () => {
         assert.ok(isValidTuning("EADGBE") === true, "EADGBE is a valid tuning.");
         assert.ok(isValidTuning("D#G#C#F#A#D#") === true, "D# G# C# F# A# D# is a valid tuning.");
         assert.ok(isValidTuning("EbAbDbGbBbEb") === true, "Eb Ab Db Gb Bb Eb is a valid tuning.");
@@ -31,18 +33,18 @@ describe("FOO BAR", () => {
         assert.ok(isValidTuning("EADGB2") === false, "EADGB2 is not a valid tuning.");
     });
 
-    it("chordictionary.tuning", () => {
+    _it("chordictionary.tuning", () => {
         assert.deepEqual(tuning.guitar.standard, ["E", "A", "D", "G", "B", "E"], "Guitar standard tuning is EADGBE.");
         assert.deepEqual(tuning.guitar.halfstepdown, ["D#", "G#", "C#", "F#", "A#", "D#"], "Guitar half-step down tuning is D#G#C#F#A#D#.");
         assert.deepEqual(tuning.guitar.open_g, ["G", "G", "D", "G", "B", "D"], "Guitar Open G tuning is GGDGBD.");
         assert.deepEqual(tuning.bass.standard, ["E", "A", "D", "G"], "Bass standard tuning is EADG.");
     });
 
-    it("chordictionary.notes", () => {
+    _it("chordictionary.notes", () => {
         assert.deepEqual(_notes, ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"], "Found all 12 notes.");
     });
 
-    it("chordictionary.parseTuning", () => {
+    _it("chordictionary.parseTuning", () => {
         assert.deepEqual(parseTuning("EADGBE"), ["E", "A", "D", "G", "B", "E"], "Parsing standard tuning.");
         assert.deepEqual(parseTuning("D#G#C#F#A#D#"), ["D#", "G#", "C#", "F#", "A#", "D#"], "Parsing tuning with sharps.");
         assert.deepEqual(parseTuning("eadgbe"), ["E", "A", "D", "G", "B", "E"], "Parsing standard tuning lowercase.");
@@ -50,7 +52,7 @@ describe("FOO BAR", () => {
         assert.ok(parseTuning("EADGBE2") === false, "EADGBE2 returns false as it cannot contains numbers.");
     });
 
-    it("chordictionary.parseTab", () => {
+    _it("chordictionary.parseTab", () => {
         assert.deepEqual(parseTab("X32010"), ["x", "3", "2", "0", "1", "0"], "Parsing X32010.");
         assert.deepEqual(parseTab("x32010"), ["x", "3", "2", "0", "1", "0"], "Parsing x32010 (lowercase.");
         assert.deepEqual(parseTab("81010988"), ["8", "10", "10", "9", "8", "8"], "Parsing 81010988.");
@@ -58,14 +60,14 @@ describe("FOO BAR", () => {
         assert.throws(() => parseTab("ABC123"), WORDING.invalidTab, "ABC123 throws an error as tab can only contains numbers or X.");
     });
 
-    it("chordictionary.parseChord", () => {
+    _it("chordictionary.parseChord", () => {
         assert.deepEqual(parseChord("C"), ["C", ""], "Parsing C.");
         assert.deepEqual(parseChord("Cmin"), ["C", "min"], "Parsing Cmin.");
         assert.deepEqual(parseChord("C#min7"), ["C#", "min7"], "Parsing C#min7.");
         assert.deepEqual(parseChord("Cm9(Maj7)"), ["C", "m9(Maj7)"], "Parsing Cm9(Maj7).");
     });
 
-    it("guitar.getChordInfo() with " + chords.length + " chords", () => {
+    _it("guitar.getChordInfo() with " + chords.length + " chords", () => {
         for (let i = 0; i < chords.length; i++) {
             let raw_results = guitar.getChordInfo(chords[i][0]);
             let results = transform(raw_results);
---
+++ spec/support/jasmine-defs.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+export function _describe(...args) {
+    describe(...args);
+}
+
+export function _it(...args) {
+    it(...args);
+}
+
